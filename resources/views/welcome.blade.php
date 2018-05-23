a46603e781e8b1ceeb393a0f3562ceca47192531
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-05-23 05:42:27 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Daivel
                 </div>
 
                 <div class="links">
